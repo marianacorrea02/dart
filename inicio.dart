dd71db8913d7bc5c2184085fe5f581b3054e8865
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-08 11:24:53 -0500

    get y set

--- inicio.dart
+++ inicio.dart
@@ -1,24 +1,26 @@
+iport 'dart:math' as math;
 void main() {
- final rawJson = {//map
-  'nombre':'tony',
-  'poder': 'dinero'
- };
- final ironman = Heroe.fromJson( rawJson );
- //Heroe  wol=new Heroe(nombre:'Logan',poder:'regeneración');
- //wol.nombre='Logan';
- //wol.poder ='regeneración';
+ final cuadrado = new Cuadrado(5);
+ print ('area: ${cuadrado.calcularArea()}');
+ print ('area: ${cuadrado.area}');// con get
+ cuadrado.area = 20;
 }
 
-class Heroe{
- String nombre;
- String poder;
- Heroe({required this.nombre, required this.poder});// con las llaves es necesario poner en la linea dos nombre: y required lo hace oblogatorio
- Heroe.fromJson(Map<String, String> json){
-  this.nombre=json['nombre']; //]??  'no tiene'; puede que reciba valor o no
+class Cuadrado {
+ double lado;
+ 
+ double get area{ //getters no se ponen los parentesis al final ()
+  return this.lado * this.lado;
  }
- @override
- String toString() {
-  return 'nombre: ${this.nombre}';
+ set area (double valor){
+  this.lado = math.sqrt(valor)
+  print('set:$valor');
+ }
+ Cuadrado ( double lado ):
+ this.lado =lado; 
+ 
+ double calcularArea(){
+  return this.lado * this.lado;
  }
 }
 
