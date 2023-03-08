1f7e3f88eceb4f0f7c7707e1d531c7778b7fa2e7
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-08 10:12:12 -0500

    funciones

--- inicio.dart
+++ inicio.dart
@@ -1,14 +1,11 @@
 void main() {
-  Map persona = {
-    // definir tipo de datos Map<String(llave), String (dato)>
-    'nombre':'Fernando',
-    'edad': 35,
-    'soltero':false,
-    true : false,
-    1:100,
-    2:500
-  };
-  print(persona['nombre'];
-  persona.addAll({3:'juan'});
-  print(persona[2];
+ nombre = 'Mariana';
+ saludar(nombre);
+ 
 }
+
+void saludar(String n){
+  //para que reciba opcional [String n = 'No name']
+  print('hola $n');
+}
+
