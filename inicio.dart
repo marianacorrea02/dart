e32183a375941673f2844a8f762fec0e285da9d1
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-08 12:15:29 -0500

    Futures

--- inicio.dart
+++ inicio.dart
@@ -1,62 +1,24 @@
-abstract class Animal {}
-
-abstract class Mamifero extends Animal {}
-
-abstract class Ave extends Animal {}
-
-abstract class Pez extends Animal {}
-
-//Mixiin cractaeirsticas especificas
-abstract class Volador {
-  void volar() => print("volador");
-}
-
-abstract class Caminantes {
-  void caminar() => print("caminante");
-}
-
-abstract class Nadador {
-  void nadar() => print("nadar");
-}
-
-class Delfin extends Mamifero with Nadador {
-  // mixins se reconocen con with
-}
-
-class Murcielago extends Mamifero with Caminantes, Volador {
-  // mixins se reconocen con with
-}
-
-class Gato extends Mamifero with Caminantes {
-  // mixins se reconocen con with
-}
-
-class Dove extends Ave with Caminantes, Volador {
-  // mixins se reconocen con with
-}
-
-class Pato extends Ave with Caminantes, Volador, Nadador {
-  // mixins se reconocen con with
-}
-
-class Tiburon extends Pez with Nadador {
-  // mixins se reconocen con with
-}
-
-class PezVolador extends Pez with Nadador, Volador {
-  // mixins se reconocen con with
-}
-
 void main() {
-  final d = new Delfin();
-  d.nadar();
+  
+  print('Antes de la petición');
+  
+  httpGet('https://api.nasa.com/aliens')
+     .then( (data) { // despues del future
+       
+       print( data.toUpperCase() );
+       
+     });
+  
+  
+  print('Fin del programa');
+    
+}
 
-  final m = new Murcielago();
-  m.caminar();
-  m.volar();
 
-  final p = new Pato();
-  p.caminar();
-  p.nadar();
-  p.volar();
+
+
+Future<String> httpGet( String url ) {// se define el tipo del Future, asi se puede usae los metodos de los stringd
+  return Future.delayed( 
+    Duration( seconds: 3 ), () =>'Hola Mundo - 3 segundos' //=> metodo de return cuando solo hay un valor
+  );
 }
