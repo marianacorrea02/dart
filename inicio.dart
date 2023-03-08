2e6a3f25fc52b790828af6bb040f09673821e337
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-08 10:00:46 -0500

    listas

--- inicio.dart
+++ inicio.dart
@@ -1,9 +1,11 @@
 void main() {
-  //puede tener valores nulos ?
-  bool? isActive = null;
-  if( isActive == null){
-    print('Esta activo');
-  }else{
-    print('Esta inactivo');
-  }
+  //listas
+  List<int> numeros =[1,2,3,4];
+  //metodos de listas
+  //si no se define el tipo de dato puede recibir lo que sea <int>
+  numeros.add(5);
+  
+  final masNumeros= List.generate(100,(int index) =>index);
+  //() => funcion que retorna el index
+  print( numeros);
 }
