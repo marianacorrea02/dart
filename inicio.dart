58095d7f2dc9bb0271bdd08b5052bf203b6ff26b
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-08 11:12:59 -0500

    Constructor con map

--- inicio.dart
+++ inicio.dart
@@ -1,13 +1,21 @@
 void main() {
- Heroe  wol=new Heroe('Logan','regeneración');
+ final rawJson = {//map
+  'nombre':'tony',
+  'poder': 'dinero'
+ };
+ final ironman = Heroe.fromJson( rawJson );
+ //Heroe  wol=new Heroe(nombre:'Logan',poder:'regeneración');
  //wol.nombre='Logan';
  //wol.poder ='regeneración';
 }
 
 class Heroe{
- String? nombre;
- String? poder;
- Heroe(this.nombre, this.poder);//inicializa es como el constructor para saltarnos la linea 3 y 4 y agregar a la linea 2
+ String nombre;
+ String poder;
+ Heroe({required this.nombre, required this.poder});// con las llaves es necesario poner en la linea dos nombre: y required lo hace oblogatorio
+ Heroe.fromJson(Map<String, String> json){
+  this.nombre=json['nombre']; //]??  'no tiene'; puede que reciba valor o no
+ }
  @override
  String toString() {
   return 'nombre: ${this.nombre}';
