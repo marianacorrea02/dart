7281522dd8ecb484b08f0ff0ca7c8d77b72c9ec8
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-09 15:48:42 -0500

    inicio screen card

--- widgwts/screens/card_screen.dart
+++ widgwts/screens/card_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/widgets/widgets.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cards'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        children: const [CustomCardType1()],
+      ),
+    );
   }
 }
