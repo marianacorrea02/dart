1d4c4ff5b52fbb3a0ad97dbc309337a1cd5e79a6
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-09 16:36:37 -0500

    diseño al card redondeado

--- widgwts/widget/custom_card_type2.dart
+++ widgwts/widget/custom_card_type2.dart
@@ -6,11 +6,15 @@ class CustomCartType2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
+      //adaptar contenido
+      clipBehavior: Clip.antiAlias,
+      //boder
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       child: Column(
         children: [
           //image normal FadeInImagen animacion mientras carga imagen
-          FadeInImage(
+          const FadeInImage(
             //poner link de imagen de google
             image: NetworkImage(
                 'https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
@@ -18,6 +22,13 @@ class CustomCartType2 extends StatelessWidget {
               raiz de proyecto crear carpeta assets para poner ahi los recursos
               pubspect.yaml agregar assets*/
             placeholder: AssetImage('assets/jar-loading.gif'),
+            //para cuando el ancho es limitado
+            width: double.infinity,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+            child: Text('texto'),
           )
         ],
       ),
