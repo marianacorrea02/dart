7e5a16f4c37f95554dd04c0e67cbb4a9f735d8ee
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-09 12:47:58 -0500

    Agregan exports

--- widgwts/screens/screens.dart
+++ widgwts/screens/screens.dart
@@ -1,1 +1,6 @@
+// se ponen los imports del main y se cambia palabr aimport por export
 
+export 'package:flutter_application_2/screens/home_screen.dart';
+export 'package:flutter_application_2/screens/alert_screen.dart';
+export 'package:flutter_application_2/screens/card_screen.dart';
+export 'package:flutter_application_2/screens/listview_screen.dart';
