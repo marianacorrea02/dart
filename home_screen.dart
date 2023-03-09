d626b4fd059cbf7c6ac6388444c2a10bc441bf73
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-09 11:35:44 -0500

    uso de ListView.separates y onTab

--- home_screen.dart
+++ home_screen.dart
@@ -1,46 +1,31 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'dart:html';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key}); // identificar widget
-  //variable propiedad
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  final options = const [
+    'Mariana',
+    'Jeisson',
+    'Adrian',
+    'Juliana',
+    'Luz'
+  ]; //crear la lista
+  const Listview1Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TextStyle title = const TextStyle(
-        fontSize: 20, color: Colors.green, fontWeight: FontWeight.bold);
-    var body = const TextStyle(fontSize: 15, color: Colors.black);
-    int c = 0;
-    //arbol de widgets
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Gruum'),
-        ),
-        backgroundColor: Colors.green,
+        body: ListView.separated(
+      // pone linea de separación
+      // pone la linea de division
+      itemBuilder: (context, index) => ListTile(
+        // estilo de separación entre items
+        //crea un widget
+        title: Text(options[index]),
+        onTap: () {}, //efecto de seleccion
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment:
-              CrossAxisAlignment.end, //alinear en fila al widger mas largo
-          mainAxisAlignment: MainAxisAlignment.center, // alinear en columna
-          children: [
-            Text('Contador de Clicks', style: title),
-            Text('$c', style: body),
-          ],
-        ),
-      ),
-      floatingActionButtonLocation:
-          FloatingActionButtonLocation.endFloat, //poscicionar button
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('hola');
-          c++;
-        },
-        backgroundColor: Colors.green,
-      ),
-    );
+      separatorBuilder: (_, index) => const Divider(),
+      itemCount: options.length, //cantidad de items en, bajo demanda
+    ));
   }
 }
