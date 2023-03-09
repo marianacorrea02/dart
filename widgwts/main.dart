2df51311b8488c8e848185b4e673130911d7e875
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-09 11:05:42 -0500

    Create main.dart

---
+++ widgwts/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/screens/listview_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Material App', home: Listview1Screen());
+  }
+}
