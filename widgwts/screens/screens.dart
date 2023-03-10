a354126b2019bcb5d14936c7aa4e5f44287c9c23
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 16:50:52 -0500

    exporta slide

--- widgwts/screens/screens.dart
+++ widgwts/screens/screens.dart
@@ -1,6 +1,5 @@
 // se ponen los imports del main y se cambia palabr aimport por export
-import 'package:flutter_application_2/screens/input_screen.dart';
-
+export 'package:flutter_application_2/screens/input_screen.dart';
 export 'package:flutter_application_2/screens/alert_screen.dart';
 export 'package:flutter_application_2/screens/card_screen.dart';
 export 'package:flutter_application_2/screens/listview_screen.dart';
