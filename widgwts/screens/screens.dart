e4d0661ca08a2f2ee3e5e3876fe9844dc62467d6
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 12:27:12 -0500

    exporta archivo input

--- widgwts/screens/screens.dart
+++ widgwts/screens/screens.dart
@@ -1,4 +1,6 @@
 // se ponen los imports del main y se cambia palabr aimport por export
+import 'package:flutter_application_2/screens/input_screen.dart';
+
 export 'package:flutter_application_2/screens/alert_screen.dart';
 export 'package:flutter_application_2/screens/card_screen.dart';
 export 'package:flutter_application_2/screens/listview_screen.dart';
