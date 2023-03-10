8ac739493d8644ef55260bc04d82b95088a81ffd
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 11:07:52 -0500

    exporta pantalla avatar

--- widgwts/screens/screens.dart
+++ widgwts/screens/screens.dart
@@ -1,5 +1,9 @@
 // se ponen los imports del main y se cambia palabr aimport por export
 
+export 'package:flutter_application_2/screens/avatar_screen.dart';
+
+export 'package:flutter_application_2/theme/app_theme.dart';
+
 export 'package:flutter_application_2/screens/home_screen.dart';
 export 'package:flutter_application_2/screens/alert_screen.dart';
 export 'package:flutter_application_2/screens/card_screen.dart';
