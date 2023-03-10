79f1f7c7655d375ed115eaec667e1a9efc1becb8
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 12:36:22 -0500

    se agrega el campo formulario

--- widgwts/screens/input_screen.dart
+++ widgwts/screens/input_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+//formularios
 class InputScreen extends StatelessWidget {
   const InputScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inputs'),
+      ),
+      //permite hacer scroll
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              //formularios mas interaccioene
+              TextFormField()
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
