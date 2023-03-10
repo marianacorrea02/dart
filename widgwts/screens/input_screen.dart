bc2c80ed69bcc44fe7ea43ddd5261394eb5fafd7
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 13:19:49 -0500

    agregar border

--- widgwts/screens/input_screen.dart
+++ widgwts/screens/input_screen.dart
@@ -27,6 +27,23 @@ class InputScreen extends StatelessWidget {
                 onChanged: (value) {
                   print('value:$value');
                 },
+                //
+                validator: (value) {
+                  if (value == null) return 'Este campo es requerido';
+                  //si tiene np cumple la validacion
+                  return value.length < 0 ? 'minimo de 3 ltras' : null;
+                },
+                //usar el validador
+                autovalidateMode: AutovalidateMode.always,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre de Usuario',
+                  labelText: 'Nombre',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          topRight: Radius.circular(10))),
+                  helperText: 'Aqui va su nombre de usuario',
+                ),
               ),
             ],
           ),
