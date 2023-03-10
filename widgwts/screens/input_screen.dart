0cdaefdf9e533204befd9ad4767d55a267b11a87
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 14:34:04 -0500

    se remplaza por widgets

--- widgwts/screens/input_screen.dart
+++ widgwts/screens/input_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 //formularios
 class InputScreen extends StatelessWidget {
   const InputScreen({super.key});
@@ -11,40 +13,23 @@ class InputScreen extends StatelessWidget {
         title: const Text('Inputs'),
       ),
       //permite hacer scroll
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             children: [
               //formularios mas interaccioene
-              TextFormField(
-                // al cambiar de pantalla se enfoca en el espacio a llenars
-                autofocus: true,
-                initialValue: '',
-                //empezar cada palabra con capital
-                textCapitalization: TextCapitalization.words,
-                //capturar el valor
-                onChanged: (value) {
-                  print('value:$value');
-                },
-                //
-                validator: (value) {
-                  if (value == null) return 'Este campo es requerido';
-                  //si tiene np cumple la validacion
-                  return value.length < 0 ? 'minimo de 3 ltras' : null;
-                },
-                //usar el validador
-                autovalidateMode: AutovalidateMode.always,
-                decoration: const InputDecoration(
-                  hintText: 'Nombre de Usuario',
-                  labelText: 'Nombre',
-                  /*border: OutlineInputBorder(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          topRight: Radius.circular(10))),*/
-
-                  helperText: 'Aqui va su nombre de usuario',
-                ),
+              CustomImputField(
+                labelText: 'nombre',
+                helperText: 'Aqui va su nombre',
+                hintText: "Nombre de usuario",
+                icon: Icons.abc,
+              ),
+              SizedBox(height: 30),
+              CustomImputField(
+                labelText: 'apellido',
+                helperText: 'Aqui va su apellido',
+                hintText: "Apellido de usuario",
               ),
             ],
           ),
