e100789e2776be0bca99a1c50f840f8b6c23ef86
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 10:32:31 -0500

    alerta en ios

--- widgwts/screens/alert_screen.dart
+++ widgwts/screens/alert_screen.dart
@@ -1,8 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
-  void displayDialog(BuildContext context) {
+  //diseño de alerta para ios
+  void displayDialogIOS(BuildContext context) {
+    //cupertino diseños de ios
+    showCupertinoDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('Texto en ios'),
+            content: const Column(
+              //el contenido determina el largo de la colimna
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("contenido de alerta"),
+                SizedBox(height: 10),
+                FlutterLogo(
+                  size: 50,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancelar'),
+              )
+            ],
+          );
+        });
+  }
+
+  //diseño de alerta para android
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
       //cerrar el dialogo al dar click en la sombra por defecto en false
       barrierDismissible: true,
@@ -39,7 +73,12 @@ class AlertScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-            onPressed: () => displayDialog(context),
+            //para poner diseño dependiendo el SO
+            /*onPressed: () => Platform.isAndroid
+                ? displayDialogAndroid(context)
+                : displayDialogIOS(context)*/
+            // funciona en web
+            onPressed: () => displayDialogAndroid(context),
             child: const Text('Mensaje de alerta')),
       ),
       floatingActionButton: FloatingActionButton(
