e46dd090fdf222551c820c82ced51b66b2c98e6e
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 15:21:20 -0500

    se agrega boton de guardar
    se agrean keys para hacer las validaciones del formulario

--- widgwts/screens/input_screen.dart
+++ widgwts/screens/input_screen.dart
@@ -8,47 +8,74 @@ class InputScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // se agregan keys
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+    //datos del form
+    final Map<String, String> formValues = {
+      'fisrt': 'Mariana',
+      'last': 'Correa',
+      'email': 'mariana@gmail.com',
+      'password': '12345',
+      'role': 'Admin',
+    };
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs'),
       ),
       //permite hacer scroll
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            children: [
-              //formularios mas interaccioene
-              CustomImputField(
-                labelText: 'nombre',
-                helperText: 'Aqui va su nombre',
-                hintText: "Nombre de usuario",
-                icon: Icons.abc,
-                textCapitalization: TextCapitalization.words,
-              ),
-              SizedBox(height: 30),
-              CustomImputField(
-                labelText: 'apellido',
-                helperText: 'Aqui va su apellido',
-                hintText: "Apellido de usuario",
-                textCapitalization: TextCapitalization.words,
-              ),
-              SizedBox(height: 30),
-              CustomImputField(
-                labelText: 'Email',
-                helperText: 'Aqui va su correo',
-                hintText: "correp de usuario",
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(height: 30),
-              CustomImputField(
-                labelText: 'Contraseña',
-                helperText: 'Aqui va su contraseña',
-                hintText: "contraseña de usuario",
-                keyboardType: TextInputType.emailAddress,
-                isPassword: true,
-              ),
-            ],
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Form(
+            //se asigna la llave
+            key: myFormKey,
+            child: Column(
+              children: [
+                //formularios mas interaccioene
+                const CustomImputField(
+                  labelText: 'nombre',
+                  helperText: 'Aqui va su nombre',
+                  hintText: "Nombre de usuario",
+                  icon: Icons.abc,
+                  textCapitalization: TextCapitalization.words,
+                ),
+                const SizedBox(height: 30),
+                const CustomImputField(
+                  labelText: 'apellido',
+                  helperText: 'Aqui va su apellido',
+                  hintText: "Apellido de usuario",
+                  textCapitalization: TextCapitalization.words,
+                ),
+                const SizedBox(height: 30),
+                const CustomImputField(
+                  labelText: 'Email',
+                  helperText: 'Aqui va su correo',
+                  hintText: "correp de usuario",
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 30),
+                const CustomImputField(
+                  labelText: 'Contraseña',
+                  helperText: 'Aqui va su contraseña',
+                  hintText: "contraseña de usuario",
+                  keyboardType: TextInputType.emailAddress,
+                  isPassword: true,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      //ocultar el teclado
+                      FocusScope.of(context).requestFocus(FocusNode());
+                      //validar estado de formulario
+                      if (!myFormKey.currentState!.validate()) {
+                        print('formulario no valido');
+                        return;
+                      }
+                    },
+                    child: const SizedBox(
+                        width: double.infinity,
+                        child: Center(child: const Text('Guardar')))),
+              ],
+            ),
           ),
         ),
       ),
