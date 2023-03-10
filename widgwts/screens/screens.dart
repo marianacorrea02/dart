5e5f7c2f3855cb58e82992b75e9db15d1df2d502
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 11:45:59 -0500

    exportar animatedScreen

--- widgwts/screens/screens.dart
+++ widgwts/screens/screens.dart
@@ -1,10 +1,7 @@
 // se ponen los imports del main y se cambia palabr aimport por export
-
-export 'package:flutter_application_2/screens/avatar_screen.dart';
-
-export 'package:flutter_application_2/theme/app_theme.dart';
-
-export 'package:flutter_application_2/screens/home_screen.dart';
 export 'package:flutter_application_2/screens/alert_screen.dart';
 export 'package:flutter_application_2/screens/card_screen.dart';
 export 'package:flutter_application_2/screens/listview_screen.dart';
+export 'package:flutter_application_2/screens/avatar_screen.dart';
+export 'package:flutter_application_2/screens/animated_screen.dart';
+export 'package:flutter_application_2/screens/home_screen.dart';
