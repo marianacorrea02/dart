ef896e075d0214ea7256074539017a5da5b3160c
marianacorrea02 <127052322+marianacorrea02@users.noreply.github.com>
2023-03-10 14:33:19 -0500

    se exporta el widget del input

--- widgwts/widget/widget.dart
+++ widgwts/widget/widget.dart
@@ -1,4 +1,6 @@
 //archivo de exportacion
+export 'package:flutter_application_2/widgets/custom_imput_field.dart';
+
 export 'package:flutter_application_2/widgets/custom_card_type2.dart';
 
 export 'package:flutter_application_2/widgets/custom_card_type1.dart';
